9f71b2496359342589b81b074fdb2566f2e32c6b
Tmarra122333 <Tmarra122333@gmail.com>
2021-12-19 23:16:29 -0500

    im lost, and scheduling classes with August

--- app.js
+++ app.js
@@ -8,3 +8,25 @@ function calculateCube(num){
     return (num**3)
 }
 console.log(calculateCube(5))
+
+// Number 3 is a Vowel
+
+function isAVowel(letter){
+  let vowels =["a", "e", "i", "o", "u"]
+      for (let i = 0; i < vowel.length; i++){
+          if (letter === vowel[i]) {
+             return true
+        }
+    }
+}
+console.log(isAvowel("a"));
+
+#4 - Function two length 
+
+function getTwoLengths(word1,word2){
+    let lengthOne= word1.length
+    let lengthTwo= word2.length
+    newArray= [lengthOne, lengthTwo]
+    return(newArray)
+}
+console.log(getTwoLengths("Hank","hippopalous"))
