7bfe83af6efd0e00aa726639e8efc0081ba0c07a
Tmarra122333 <Tmarra122333@gmail.com>
2021-12-19 22:50:03 -0500

    Commit 2 - Cube Calculation.

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
-An Argument is the actual value in a function - Paramater is a Placeholder for the value. 
-Console.log is used to confirm if the function is working.  Return - returns values from only the called function.
-Function will return the Value, and only the value.  
+// An Argument is the actual value in a function - Paramater is a Placeholder for the value. 
+// Console.log is used to confirm if the function is working.  Return - returns values from only the called function.
+// Function will return the Value, and only the value.  
+
+//Number 2 Calculate the cube
+
+function calculateCube(num){
+    return (num**3)
+}
+console.log(calculateCube(5))
